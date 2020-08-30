ac74fb6dad2c7dce1a9fcc85f6ffc3de254d61e8
Mikhail Cherepanov <droof1995@gmail.com>
2020-08-30 14:16:33 +0300

    api test

--- routes/api.php
+++ routes/api.php
@@ -14,24 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('login', 'Api\Controllers\AuthController@login'); //params - login password
-Route::post('register', 'Api\Controllers\AuthController@registration'); //params - login password
+Route::post('login', 'Api\Controllers\AuthController@login');
+Route::post('register', 'Api\Controllers\AuthController@registration');
 
-Route::get('categories', 'Api\Controllers\CategoryController@getAllCategory'); //without params
-Route::get('categories/items', 'Api\Controllers\ItemController@getItemsFromCategory'); //without params
+Route::get('categories', 'Api\Controllers\CategoryController@getAllCategory');
+Route::get('categories/items', 'Api\Controllers\ItemController@getItemsFromCategory');
 Route::get('item', 'Api\Controllers\ItemController@getItem');
 
 Route::group(['middleware' => ['jwt.verify']], function (){
     Route::prefix('categories')->group(function () {
-        Route::post('/create', 'Api\Controllers\CategoryController@createCategory'); //params - title post(текст поста)
-        Route::delete('/delete', 'Api\Controllers\CategoryController@deleteCategory'); //params - name post_id
-        Route::put('/update', 'Api\Controllers\CategoryController@updateCategory'); //params - (name post_id - general), (title post - additional)
+        Route::post('/create', 'Api\Controllers\CategoryController@createCategory');
+        Route::delete('/delete', 'Api\Controllers\CategoryController@deleteCategory');
+        Route::put('/update', 'Api\Controllers\CategoryController@updateCategory');
     });
 
     Route::prefix('items')->group(function () {
         Route::put('/addcategory', 'Api\Controllers\CategoryController@addCategoryToItem');
-        Route::post('/create', 'Api\Controllers\ItemController@createItem'); //params - (login password email - general), (about, role - additional)
-        Route::delete('/delete', 'Api\Controllers\ItemController@deleteItem'); //params - имя пользователя из колонки name табл users
-        Route::put('/update', 'Api\Controllers\ItemController@updateItem'); //params (name - general) (password email about role - additional)
+        Route::post('/create', 'Api\Controllers\ItemController@createItem');
+        Route::delete('/delete', 'Api\Controllers\ItemController@deleteItem');
+        Route::put('/update', 'Api\Controllers\ItemController@updateItem');
     });
 });
